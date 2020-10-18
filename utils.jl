a8094aa8c3b39283bdb60b35337bc39bb56ac6e0
abhishalya <abhinavkaushlya@gmail.com>
2020-10-18 12:50:39 +0000

    Deploying to master from  @ dbb8703e974bdbd09fe00553cf6824a519ca6c9c 🚀

---
+++ utils.jl
@@ -0,0 +1,74 @@
+using Dates
+
+"""
+    {{blogposts}}
+
+Plug in the list of blog posts contained in the `/blog/` folder.
+"""
+function hfun_blogposts()
+    today = Dates.today()
+    curyear = year(today)
+    curmonth = month(today)
+    curday = day(today)
+    list = readdir("blog")
+    reverse!(list)
+    filter!(f -> endswith(f, ".md"), list)
+    io = IOBuffer()
+    write(io, """<ul class="blog-posts">""")
+    for (i, post) in enumerate(list)
+        if post == "index.md"
+            continue
+        end
+        ps  = splitext(post)[1]
+        write(io, "<li><span><i>")
+        url = "/blog/$ps/"
+        surl = strip(url, '/')
+        title = pagevar(surl, :title)
+        pubdate = pagevar(surl, :published)
+        if isnothing(pubdate)
+            date    = "$curyear-$curmonth-$curday"
+        else
+            date    = Date(pubdate, dateformat"d U Y")
+        end
+        write(io, """$date</i></span><a href="$url">$title</a>""")
+    end
+    write(io, "</ul>")
+    return String(take!(io))
+end
+
+"""
+    {{custom_taglist}}
+
+Plug in the list of blog posts with the given tag
+"""
+function hfun_custom_taglist()::String
+    tag = locvar(:fd_tag)
+    rpaths = globvar("fd_tag_pages")[tag]
+    sorter(p) = begin
+        pubdate = pagevar(p, :published)
+        if isnothing(pubdate)
+            return Date(Dates.unix2datetime(stat(p * ".md").ctime))
+        end
+        return Date(pubdate, dateformat"d U Y")
+    end
+    sort!(rpaths, by=sorter, rev=true)
+
+    io = IOBuffer()
+    write(io, """<ul class="blog-posts">""")
+    # go over all paths
+    for rpath in rpaths
+        write(io, "<li><span><i>")
+        url = get_url(rpath)
+        title = pagevar(rpath, :title)
+        pubdate = pagevar(rpath, :published)
+        if isnothing(pubdate)
+            date    = "$curyear-$curmonth-$curday"
+        else
+            date    = Date(pubdate, dateformat"d U Y")
+        end
+        # write some appropriate HTML
+        write(io, """$date</i></span><a href="$url">$title</a>""")
+    end
+    write(io, "</ul>")
+    return String(take!(io))
+end
